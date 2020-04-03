d790e9d816f2f0f4416df4d3feadaaf876cc9795
Evgenii Danilin <evgenii.danilin.m@gmail.com>
2020-04-03 21:15:51 +0400

    battery + brightness

--- theme/widgets/battery/battery.lua
+++ theme/widgets/battery/battery.lua
@@ -24,9 +24,9 @@ end
 init = function ( theme, panel )
     theme.widget_battery = theme.dir .. "/widgets/battery/icons/battery.png"
     theme.widget_ac = theme.dir .. "/widgets/battery/icons/ac.png"
-    local _value = 0
+    local _value = {}
     local _icon = wibox.widget.imagebox(theme.widget_battery)
-    local _widget = wibox.widget {
+    local _battery = wibox.widget {
         _icon,
         value = 0,
         min_value = 0,
@@ -38,11 +38,31 @@ init = function ( theme, panel )
         start_angle = 0.5 * math.pi,
         widget = wibox.container.arcchart,
     }
+    local _brightness = wibox.widget {
+        _battery,
+        value = 0,
+        min_value = 0,
+        max_value = 100,
+        thickness = 1,
+        border_width = 0,
+        bg = theme.bg_normal,
+        colors = {'#ffffff'},
+        start_angle = 0.5 * math.pi,
+        widget = wibox.container.arcchart,
+    }
+    awful.widget.watch("xbacklight -get", 5, function(widget, stdout)
+        if tonumber(stdout) then
+            _brightness.value = tonumber(stdout)
+        end
+    end)
     awful.tooltip {
-        objects = { _widget },
+        objects = { _battery },
         align = "bottom_left",
         timer_function = function()
-            return "Battery: " .. _value .. "%"
+            if not _value.perc then
+                _value.perc = 0
+            end
+            return "Battery: " .. _value.perc .. "%"
         end,
     }
     lain.widget.bat({
@@ -51,17 +71,40 @@ init = function ( theme, panel )
         settings = function()
             if bat_now.status and bat_now.status ~= "N/A" then
                 _icon:set_image(theme.widget_battery)
-                _value = bat_now.perc
-                _widget.value = bat_now.perc
-                _widget.colors = {color(bat_now.perc)}
+                _value = bat_now
+                _battery.value = bat_now.perc
+                _battery.colors = {color(bat_now.perc)}
                 if bat_now.ac_status == 1 then
                     _icon:set_image(theme.widget_ac)
-                    _widget.colors = {'#ffffff'}
+                    _battery.colors = {'#ffffff'}
                 end
             end
         end
     })
-    return _widget
+    _brightness:buttons(awful.util.table.join(
+        awful.button({}, 1, function ()
+            awful.spawn.easy_async("xbacklight -set 50", function ( )
+                awful.spawn.easy_async('xbacklight -get', function(stdout)
+                    _brightness.value = tonumber(stdout)
+                end)
+            end)
+        end),
+        awful.button({}, 4, function ()
+            awful.spawn.easy_async("xbacklight -inc 5", function ( )
+                awful.spawn.easy_async('xbacklight -get', function(stdout)
+                    _brightness.value = tonumber(stdout)
+                end)
+            end)
+        end),
+        awful.button({}, 5, function ()
+            awful.spawn.easy_async("xbacklight -dec 5", function ( )
+                awful.spawn.easy_async('xbacklight -get', function(stdout)
+                    _brightness.value = tonumber(stdout)
+                end)
+            end)
+        end)
+    ))
+    return _brightness
 end
 
 return init
