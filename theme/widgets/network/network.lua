31e6ce0711b215a9753d24c5fe5d1850ffa681d4
Evgenii Danilin <evgenii.danilin.m@gmail.com>
2020-04-03 17:40:27 +0400

    widget network fancy

--- theme/widgets/network/network.lua
+++ theme/widgets/network/network.lua
@@ -9,35 +9,40 @@ init = function ( theme )
     theme.widget_net        = theme.dir .. "/widgets/network/icons/net.png"
     theme.widget_net_wired  = theme.dir .. "/widgets/network/icons/net_wired.png"
     local _icon = wibox.widget.imagebox(theme.widget_net)
-    local _widget = lain.widget.net {
+    local _widget = wibox.widget {
+        _icon,
+        value = 30,
+        min_value = 30,
+        max_value = 67,
+        thickness = 1,
+        border_width = 0,
+        bg = theme.bg_normal,
+        colors = {'#ffffff'},
+        start_angle = 0.5 * math.pi,
+        visible = false,
+        widget = wibox.container.arcchart,
+    }
+    lain.widget.net {
         notify = "off",
         wifi_state = "on",
         eth_state = "on",
         settings = function()
+            _widget.visible = true
             local eth0 = net_now.devices.eth0
             if eth0 then
                 if eth0.ethernet then
-                    eth_icon:set_image(theme.widget_net_wired)
-                else
-                    eth_icon:set_image()
+                    _icon:set_image(theme.widget_net_wired)
                 end
             end
             local wlan0 = net_now.devices.wlp2s0
             if wlan0 then
                 if wlan0.wifi then
-                    local signal = wlan0.signal
-                    widget:set_markup(markup.font(theme.font, string.format("%03d", signal) .. " dBm"))
-                else
-                    widget:set_markup(markup.font(theme.font, "X"))
+                    _widget.value = 0 - wlan0.signal
                 end
             end
         end
     }
-    return wibox.widget {
-        _icon,
-        _widget,
-        layout  = wibox.layout.align.horizontal
-    }
+    return _widget
 end
 
 return init
