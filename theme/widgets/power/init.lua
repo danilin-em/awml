b4480a15ac09b12669dfca7fd6eada8e0061ae52
Evgenii Danilin <evgenii.danilin.m@gmail.com>
2020-04-11 11:41:55 +0400

    add awesome quit button. FIX #6

--- theme/widgets/power/init.lua
+++ theme/widgets/power/init.lua
@@ -11,6 +11,7 @@ init = function ( theme, panel )
     theme.widget_reload_awesome = theme.dir .. "/widgets/power/icons/reload_awesome.png"
     theme.widget_restart = theme.dir .. "/widgets/power/icons/restart.png"
     theme.widget_sleep = theme.dir .. "/widgets/power/icons/sleep.png"
+    theme.widget_quit_awesome = theme.dir .. "/widgets/power/icons/exit.png"
     local _icon = wibox.widget.imagebox(theme.widget_power_menu)
     local _popup = awful.popup {
         widget = {
@@ -26,6 +27,20 @@ init = function ( theme, panel )
                     forced_height = 36,
                     buttons = awful.util.table.join(
                         awful.button({}, 1, awesome.restart)
+                    ),
+                    widget  = wibox.container.margin
+                },
+                {
+                    {
+                        image  = theme.widget_quit_awesome,
+                        resize = true,
+                        widget = wibox.widget.imagebox
+                    },
+                    margins = 5,
+                    forced_width = 36,
+                    forced_height = 36,
+                    buttons = awful.util.table.join(
+                        awful.button({}, 1, awesome.quit)
                     ),
                     widget  = wibox.container.margin
                 },
