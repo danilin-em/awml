43dbca796c68b4d10342d33c554ae09a67363883
Evgenii Danilin <evgenii.danilin.m@gmail.com>
2020-04-14 15:36:44 +0400

    cpu & mem pass args

--- services/usage/cpu.lua
+++ services/usage/cpu.lua
@@ -6,20 +6,16 @@ local cpu = require("lain.widget.cpu")
 
 local default = {
     id = 'main',
-    timeout = 2,
 }
 
 return function ( args )
     args = args or {}
     args.id = tostring(args.id or default.id)
-    args.timeout = tonumber(args.timeout or default.timeout)
     local signal = 'service:usage:cpu:'..args.id
-    local service = cpu({
-        timeout = args.timeout,
-        settings = function()
-            awesome.emit_signal(signal..':value', cpu_now)
-        end
-    })
+    args.settings = function()
+        awesome.emit_signal(signal..':value', cpu_now)
+    end
+    local service = cpu(args)
     awesome.emit_signal(signal..':init', service)
     awesome.connect_signal(signal..':update', service.update)
     return service
--- services/usage/mem.lua
+++ services/usage/mem.lua
@@ -6,20 +6,16 @@ local mem = require("lain.widget.mem")
 
 local default = {
     id = 'main',
-    timeout = 2,
 }
 
 return function ( args )
     args = args or {}
     args.id = tostring(args.id or default.id)
-    args.timeout = tonumber(args.timeout or default.timeout)
     local signal = 'service:usage:mem:'..args.id
-    local service = mem({
-        timeout = args.timeout,
-        settings = function()
-            awesome.emit_signal(signal..':value', mem_now)
-        end
-    })
+    args.settings = function()
+        awesome.emit_signal(signal..':value', mem_now)
+    end
+    local service = mem(args)
     awesome.emit_signal(signal..':init', service)
     awesome.connect_signal(signal..':update', service.update)
     return service
