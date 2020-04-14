a322af3082d82b2fb24112b1782821b76e55d5d5
Evgenii Danilin <evgenii.danilin.m@gmail.com>
2020-04-14 14:08:04 +0400

    clock widget drop watch, use native

--- theme/widgets/clock/init.lua
+++ theme/widgets/clock/init.lua
@@ -1,29 +1,22 @@
 -- clock
-local awful = require("awful")
 local wibox = require("wibox")
-local lain = require("lain")
-local markup = lain.util.markup
+local tooltip = require("awful.tooltip")
 
 init = function ( theme )
     -- Textclock
-    local _widget = awful.widget.watch(
-        "date +'%a %d %b (%m) %R'", 60,
-        function(widget, stdout)
-            widget:set_markup(" " .. markup.font(theme.font, stdout))
-        end
-    )
-    -- Calendar
-    theme.cal = lain.widget.cal({
-        attach_to = { _widget },
-        notification_preset = {
-            font = "Terminus 10",
-            fg   = theme.fg_normal,
-            bg   = theme.bg_normal
-        }
-    })
-    return wibox.widget {
-        _widget,
-        layout  = wibox.layout.align.horizontal
+    local textclock = wibox.widget.textclock('%H:%M', 30)
+    local _widget = wibox.widget {
+        textclock,
+        margins = 0,
+        widget = wibox.container.margin,
     }
+    tooltip {
+        objects = { _widget },
+        align = "bottom_left",
+        timer_function = function()
+            return os.date('%Y-%m-%d\n%A %B')
+        end,
+    }
+    return _widget
 end
 return init
