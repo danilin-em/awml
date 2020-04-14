3bfef1071eacd82884556351ff5877b0908ff5c6
Evgenii Danilin <evgenii.danilin.m@gmail.com>
2020-04-14 21:05:18 +0400

    WARN! Not stable! Drop control from widget

--- services/brightness.lua
+++ services/brightness.lua
@@ -3,40 +3,89 @@ local awesome = awesome
 
 local watch = require("awful.widget.watch")
 local easy_async = require("awful.spawn").easy_async
+local dump = require("gears.debug").dump
 
 local default = {
     id = 'main',
     timeout = 5
 }
 
+local calc_brightness = function ( )
+    local min_brightness = 5
+    local max_brightness = 100
+    local max_birghtness_time = 6
+    local hour = tonumber(os.date("%H.%M"))
+    return min_brightness + ((hour + max_birghtness_time) % 24) * (max_brightness - min_brightness) / 24
+end
+
 return function ( args )
     args = args or {}
     args.id = tostring(args.id or default.id)
     args.timeout = tonumber(args.timeout or default.timeout)
     local signal = 'service:brightness:'..args.id
-    local service = watch("xbacklight -get", args.timeout, function(_, stdout)
-        local value = tonumber(stdout) or 100
-        awesome.emit_signal(signal..':value', value)
-    end)
-    function service.inc( self, value )
-        easy_async('xbacklight -inc '.. tostring(value), function(stdout)
+    -- Service
+    local service = {
+        widget = watch("xbacklight -get", args.timeout, function(_, stdout)
+            local value = tonumber(stdout) or 100
+            awesome.emit_signal(signal..':watch', value)
+        end),
+        auto = false,
+        value = 100,
+    }
+    function service.cmd( self, args )
+        self:set_auto(false)
+        easy_async('xbacklight '.. tostring(args), function(stdout)
             easy_async('xbacklight -get', function(stdout)
-                local value = tonumber(stdout) or 100
-                awesome.emit_signal(signal..':value', value)
+                self.value = tonumber(stdout) or 100
+                awesome.emit_signal(signal..':props', self)
             end)
         end)
+    end
+    function service.set( self, value )
+        self:cmd('-set '..tostring(value))
+    end
+    function service.inc( self, value )
+        self:cmd('-inc '..tostring(value))
     end
     function service.dec( self, value )
-        easy_async('xbacklight -dec '.. tostring(value), function(stdout)
-            easy_async('xbacklight -get', function(stdout)
-                local value = tonumber(stdout) or 100
-                awesome.emit_signal(signal..':value', value)
-            end)
-        end)
+        self:cmd('-dec '..tostring(value))
     end
+    function service.set_auto( self, value )
+        awesome.emit_signal(signal..':auto', value)
+        self.auto = value
+    end
+    function service.is_auto( self )
+        return self.auto
+    end
+    function service.auto_toggle( self )
+        self:set_auto(not self:is_auto())
+    end
+    -- Connect Signals
+    awesome.connect_signal(signal..':watch', function(value)
+        if service:is_auto() then
+            awesome.emit_signal(signal..':set', calc_brightness())
+        else
+            service.value = value
+            awesome.emit_signal(signal..':props', service)
+        end
+    end)
+    awesome.connect_signal(signal..':auto_toggle', function()
+        service:auto_toggle()
+        awesome.emit_signal(signal..':props', service)
+    end)
+    awesome.connect_signal(signal..':set', function(value)
+        service:set(value)
+    end)
+    awesome.connect_signal(signal..':inc', function(value)
+        service:inc(value)
+    end)
+    awesome.connect_signal(signal..':dec', function(value)
+        service:dec(value)
+    end)
     awesome.connect_signal(signal..':sync', function(name, value)
         awesome.emit_signal(signal..':sync>'..tostring(name), value)
     end)
+    -- Init Signal
     awesome.emit_signal(signal..':init', service)
     return service
 end
--- theme/widgets/brightness/init.lua
+++ theme/widgets/brightness/init.lua
@@ -1,27 +1,17 @@
 -- brightness
+local awesome = awesome
 
 local awful = require("awful")
 local wibox = require("wibox")
-local lain = require("lain")
-
-local calc_brightness = function ( )
-    local min_brightness = 5
-    local max_brightness = 100
-    local max_birghtness_time = 6
-    local hour = tonumber(os.date("%H.%M"))
-    return min_brightness + ((hour + max_birghtness_time) % 24) * (max_brightness - min_brightness) / 24
-end
 
 init = function ( theme )
     theme.widget_brightness_auto = theme.dir .. "/widgets/brightness/icons/brightness_auto.png"
     theme.widget_brightness_high = theme.dir .. "/widgets/brightness/icons/brightness_high.png"
     theme.widget_brightness_medium = theme.dir .. "/widgets/brightness/icons/brightness_medium.png"
     theme.widget_brightness_low = theme.dir .. "/widgets/brightness/icons/brightness_low.png"
-    local _icon = wibox.widget.imagebox(theme.widget_brightness_low)
     local _brightness = wibox.widget {
-        _icon,
+        wibox.widget.imagebox(theme.widget_brightness_low),
         value = 0,
-        auto_value = true,
         min_value = 0,
         max_value = 100,
         thickness = 1,
@@ -32,58 +22,30 @@ init = function ( theme )
         start_angle = 0.5 * math.pi,
         widget = wibox.container.arcchart,
     }
-    function _brightness.update_auto_value( value )
-        _brightness.auto_value = value
-        awesome.emit_signal('service:brightness:main:sync', 'auto', value)
-    end
-    function _brightness.update_value( value )
-        local set = function ( value )
-            _brightness.value = value
-            if _brightness.auto_value then
-                _icon:set_image(theme.widget_brightness_auto)
-                value = calc_brightness()
-                awful.spawn.easy_async("xbacklight -set "..value, function (stdout) end)
-            elseif value <= 30 then
-                _icon:set_image(theme.widget_brightness_low)
-            elseif value <= 60 then
-                _icon:set_image(theme.widget_brightness_medium)
-            else
-                _icon:set_image(theme.widget_brightness_high)
-            end
-            _brightness.value = value
-        end
-        if value then
-            set(value)
+    function _brightness.update( self, props )
+        self.value = props.value
+        if props:is_auto() then
+            self._private.widget:set_image(theme.widget_brightness_auto)
+        elseif props.value <= 30 then
+            self._private.widget:set_image(theme.widget_brightness_low)
+        elseif props.value <= 60 then
+            self._private.widget:set_image(theme.widget_brightness_medium)
         else
-            awful.spawn.easy_async('xbacklight -get', function(stdout)
-                local value = tonumber(stdout) or 0
-                set(value)
-            end)
+            self._private.widget:set_image(theme.widget_brightness_high)
         end
     end
-    awesome.connect_signal('service:brightness:main:value', function(value)
-        _brightness.update_value(value)
-    end)
-    awesome.connect_signal('service:brightness:main:sync>auto', function(value)
-        _brightness.auto_value = value
-        _brightness.update_value(false)
+    awesome.connect_signal('service:brightness:main:props', function(props)
+        _brightness:update(props)
     end)
     _brightness:buttons(awful.util.table.join(
         awful.button({}, 1, function ()
-            _brightness.update_auto_value(not _brightness.auto_value)
-            _brightness.update_value(false)
+            awesome.emit_signal('service:brightness:main:auto_toggle')
         end),
         awful.button({}, 4, function ()
-            _brightness.update_auto_value(false)
-            awful.spawn.easy_async("xbacklight -inc 5", function (stdout) 
-                _brightness.update_value(false)
-            end)
+            awesome.emit_signal('service:brightness:main:inc', 5)
         end),
         awful.button({}, 5, function ()
-            _brightness.update_auto_value(false)
-            awful.spawn.easy_async("xbacklight -dec 5", function (stdout) 
-                _brightness.update_value(false)
-            end)
+            awesome.emit_signal('service:brightness:main:dec', 5)
         end)
     ))
     return _brightness
