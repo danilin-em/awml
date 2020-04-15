85358c690d3121945733c045275b586153e4ff4c
Evgenii Danilin <evgenii.danilin.m@gmail.com>
2020-04-15 17:03:48 +0400

    network cs

--- services/network.lua
+++ services/network.lua
@@ -13,7 +13,7 @@ return function ( args )
     args.id = tostring(args.id or default.id)
     local signal = 'service:network:'..args.id
     args.settings = function()
-        awesome.emit_signal(signal..':value', net_now)
+        awesome.emit_signal(signal..':value', net_now) -- luacheck: ignore
     end
     local service = net(args)
     awesome.emit_signal(signal..':init', service)
