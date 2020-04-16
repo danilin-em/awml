103b2901e3972d14bbde6d51a2059f17824eaa04
Evgenii Danilin <evgenii.danilin.m@gmail.com>
2020-04-16 18:55:47 +0400

    base labels

--- theme/widgets/power/init.lua
+++ theme/widgets/power/init.lua
@@ -11,10 +11,15 @@ return function ( theme, screen )
     theme.widget_power_sleep = theme.dir .. "/widgets/power/icons/sleep.png"
     theme.widget_power_quit_awesome = theme.dir .. "/widgets/power/icons/exit.png"
     theme.widget_power_lock = theme.dir .. "/widgets/power/icons/lock.png"
-    local button = function ( image, buttons )
+    local button = function ( title, image, buttons )
         local margin = 20
         local background = wibox.container.background(
-            wibox.container.margin(wibox.widget.imagebox(image, false), margin, margin, margin, margin)
+            wibox.container.margin(
+                wibox.layout.fixed.vertical(
+                    wibox.widget.imagebox(image, false),
+                    wibox.container.place(wibox.widget.textbox(title))
+                ),
+            margin, margin, margin, margin)
         )
         background:connect_signal('mouse::enter', function ( curr )
             curr.bg = theme.bg_focus
@@ -30,32 +35,32 @@ return function ( theme, screen )
     _icon.bg = theme.bg_normal
     local _popup = wibox{
         widget = wibox.widget {
-            button(theme.widget_power_reload_awesome, awful.util.table.join(
+            button('Reload Awesome', theme.widget_power_reload_awesome, awful.util.table.join(
                 awful.button({}, 1, awesome.restart)
             )),
-            button(theme.widget_power_lock, awful.util.table.join(
+            button('Lock', theme.widget_power_lock, awful.util.table.join(
                 awful.button({}, 1, function ( )
                     awful.spawn.with_shell("dm-tool lock") -- TODO: Use global lock command. Linked by #23
                 end)
             )),
-            button(theme.widget_power_quit_awesome, awful.util.table.join(
+            button('Quit', theme.widget_power_quit_awesome, awful.util.table.join(
                 awful.button({}, 1, function ( )
                     awesome.quit()
                 end)
             )),
-            button(theme.widget_power_sleep, awful.util.table.join(
+            button('Sleep', theme.widget_power_sleep, awful.util.table.join(
                 awful.button({}, 1, function ( )
-                    awful.spawn.with_shell("systemctl suspend")
+                    awful.spawn.with_shell("systemctl suspend") -- TODO: Store this actions in to Power Service
                 end)
             )),
-            button(theme.widget_power_restart, awful.util.table.join(
+            button('Restart', theme.widget_power_restart, awful.util.table.join(
                 awful.button({}, 1, function ( )
-                    awful.spawn.with_shell("systemctl reboot")
+                    awful.spawn.with_shell("systemctl reboot") -- TODO: Store this actions in to Power Service
                 end)
             )),
-            button(theme.widget_power_power_off, awful.util.table.join(
+            button('Power Off', theme.widget_power_power_off, awful.util.table.join(
                 awful.button({}, 1, function ( )
-                    awful.spawn.with_shell("systemctl poweroff")
+                    awful.spawn.with_shell("systemctl poweroff") -- TODO: Store this actions in to Power Service
                 end)
             )),
             layout = wibox.layout.flex.horizontal,
