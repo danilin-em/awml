841489a786be820c2dc74e69e94d440d1384138f
Evgenii Danilin <evgenii.danilin.m@gmail.com>
2020-04-18 21:16:26 +0400

    state swicher

--- theme/widgets/pomodoro/init.lua
+++ theme/widgets/pomodoro/init.lua
@@ -1,17 +1,28 @@
 -- pomodoro
 local wibox = require("wibox")
 local tooltip = require("awful.tooltip")
+local button = require("awful.button")
+local table_join = require("awful.util").table.join
 
 return function ( theme ) -- luacheck: no unused
     theme.widget_pomodoro_timer_stopped = theme.dir .. "/widgets/pomodoro/icons/timer_stopped.png"
     theme.widget_pomodoro_timer_active = theme.dir .. "/widgets/pomodoro/icons/timer_active.png"
     theme.widget_pomodoro_timer_break = theme.dir .. "/widgets/pomodoro/icons/timer_break.png"
     local _icon = wibox.widget.imagebox(theme.widget_pomodoro_timer_stopped)
+    function _icon.set_state (self, state)
+        local states = {
+            stopped = theme.widget_pomodoro_timer_stopped,
+            active = theme.widget_pomodoro_timer_active,
+            short_break = theme.widget_pomodoro_timer_break,
+            long_break = theme.widget_pomodoro_timer_break,
+        }
+        self:set_image(states[state])
+    end
     local _widget = wibox.widget {
         _icon,
         value = 0,
         min_value = 0,
-        max_value = 100,
+        max_value = 1500, -- Seconds
         thickness = 1,
         border_width = 0,
         bg = theme.bg_normal,
@@ -26,17 +37,38 @@ return function ( theme ) -- luacheck: no unused
             long_break = 'Good job! Take a long break',
         },
         state_line = {
-            'stopped',
-            'active',
-            'short_break',
-            'active',
-            'long_break',
+            'stopped', -- 1
+            'active', -- 2
+            'short_break', -- 3
+            'active', -- 4
+            'long_break', -- 5
         },
         state_current = 1,
     }
+    function _widget.state_name (self)
+        return self.state_line[self.state_current]
+    end
     function _widget.state_text (self)
-        local state = self.state_line[self.state_current]
+        local state = self:state_name()
         return self.states[state]
+    end
+    function _widget.set_state (self, index)
+        if index > #self.state_line then
+            index = 1
+        end
+        self.state_current = index
+        return index
+    end
+    function _widget.inc_state (self)
+        return self:set_state(self.state_current + 1)
+    end
+    function _widget.Start (self)
+        self:inc_state()
+        _icon:set_state(self:state_name())
+    end
+    function _widget.Stop (self)
+        self:set_state(1)
+        _icon:set_state(self:state_name())
     end
     tooltip {
         objects = { _widget },
@@ -45,5 +77,19 @@ return function ( theme ) -- luacheck: no unused
             return _widget:state_text()
         end,
     }
+    awesome.connect_signal('widget:pomodoro:Start', function ( )
+        _widget:Start()
+    end)
+    awesome.connect_signal('widget:pomodoro:Stop', function ( )
+        _widget:Stop()
+    end)
+    _widget:buttons(table_join(
+        button({}, 1, function ()
+            awesome.emit_signal('widget:pomodoro:Start')
+        end),
+        button({}, 3, function ()
+            awesome.emit_signal('widget:pomodoro:Stop')
+        end)
+    ))
     return _widget
 end
